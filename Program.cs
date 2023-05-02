8705fde6b9aa0c61a1603970e00dcf30013ce701
WadBike <73782170+WadBike@users.noreply.github.com>
2023-05-02 19:07:23 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Internal;
+
+namespace secondMay2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Character c = new Character(100);
+            Player player1 = new Player(100);
+            Enemy enemy1 = new Enemy(100);
+
+            Console.WriteLine(enemy1.levelUp());
+        }
+
+        class Character
+        {
+            private int health;
+
+            public Character(int health)
+            {
+                this.health = health;
+            }
+
+            public int modifyHealth(int modifier)
+            {
+                health += modifier;
+                return health;
+            }
+
+            public int getHealth()
+            {
+                return health;
+            }
+        }
+
+        class Player : Character
+        {
+            public Player(int health) : base(health)
+            {
+
+            }
+
+        }
+
+        class Enemy : Character
+        {
+            private int level = 1;
+            public Enemy(int health) : base(health)
+            {
+
+            }
+
+            public int levelUp()
+            {
+                return level++;
+            }
+        }
+    }
+}
 
