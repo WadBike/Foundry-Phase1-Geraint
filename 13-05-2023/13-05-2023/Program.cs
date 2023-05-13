49fd8cbf08e723059deef3516f39f3bc1658d0e9
WadBike <73782170+WadBike@users.noreply.github.com>
2023-05-13 11:49:11 +0100

    Added abstract class exercise

--- 13-05-2023/13-05-2023/Program.cs
+++ 13-05-2023/13-05-2023/Program.cs
@@ -4,14 +4,16 @@
     {
         static void Main(string[] args)
         {
-
+            Dog d1 = new Dog("Rover");
+            Cat c1 = new Cat("Garfield");
             /*Book b1 = new Book("Lord of the Rings", 1178, Material.Hardback);
             Book b2 = new Book("The Hobbit", 2000, Material.Paperback);
-            */
+            
             VideoGame v1 = new VideoGame("PewPew", 30, Genre.Shooter);
             VideoGame v2 = new VideoGame("Wanderer", 40, Genre.RPG);
             VideoGame v3 = new VideoGame("Zoomies", 25, Genre.SimRacer);
-            Console.WriteLine(v2.Genre);
+            Console.WriteLine(v2.Genre);*/
+            Console.WriteLine(c1.Affection);
 
         }
     }
@@ -37,7 +39,7 @@
             Material = material;
         }*/
 
-    enum Genre
+    /*enum Genre
     {
         Shooter,
         RPG,
@@ -55,6 +57,62 @@
             Name = name;
             Price = price;
             Genre = genre;
+        }*/
+
+    abstract class Animal
+    {
+        public string Name { get; set; }
+        public abstract int Speed { get; set; }
+        public abstract string Affection { get; set; }
+
+        public Animal(string name)
+        {
+            Name = name;
+        }
+
+        
+        public abstract void AnimalSound();
+
+        public void sleep()
+        {
+            Console.WriteLine("ZzzZzZz");
+        }
+
+    }
+
+    class Dog : Animal
+    {
+        public override int Speed { get; set; }
+
+        public override string Affection { get; set; }
+
+        public Dog(string name) : base(name)
+        {
+            Speed = 10;
+            Affection = "Very affectionate";
+        }
+
+        public override void AnimalSound()
+        {
+            Console.WriteLine("WOOF");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override string Affection { get; set; }
+
+        public override int Speed { get; set; }
+
+        public Cat(string name) : base(name)
+        {
+            Speed = 5;
+            Affection = "Hates you and everything you stand for.";
+        }
+
+        public override void AnimalSound()
+        {
+            Console.WriteLine("MEOW");
         }
     }
 }
